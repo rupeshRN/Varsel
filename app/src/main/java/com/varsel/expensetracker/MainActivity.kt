da3724fa886209e6ee02120844f84985961421ef
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-07 20:47:40 +0530

    Update MainActivity.kt
    Fix import
    
    [skip ci]

--- app/src/main/java/com/varsel/expensetracker/MainActivity.kt
+++ app/src/main/java/com/varsel/expensetracker/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.varsel.expensetracker.ui.navigation.AppNavGraph // Replace with your actual navigation graph package
+import com.varsel.expensetracker.ui.navigation.NavGraph // Replace with your actual navigation graph package
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
