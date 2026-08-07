bf3e047928ad4b999a5584350da52472a9268bea
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-07 11:03:32 +0530

    Delete gradle/build.gradle.kts
    Move to root
    
    [skip ci]

--- gradle/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-// Top-level build file where common options across all modules are declared.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.hilt.android) apply false
-    alias(libs.plugins.ksp) apply false
-}
