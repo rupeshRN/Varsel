6df5afc28da91c9c42780c9f1ee5950169af7992
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-08 00:27:24 +0530

    Update TransactionRepository.kt
    [skip ci]

--- app/src/main/java/com/varsel/expensetracker/domain/repository/TransactionRepository.kt
+++ app/src/main/java/com/varsel/expensetracker/domain/repository/TransactionRepository.kt
@@ -1,12 +1,13 @@
 package com.varsel.expensetracker.domain.repository
 
 import com.varsel.expensetracker.domain.model.Transaction
-import com.varsel.expensetracker.domain.model.TransactionType
 import kotlinx.coroutines.flow.Flow
 
 interface TransactionRepository {
     fun getAllTransactions(): Flow<List<Transaction>>
-    fun getTransactionsByType(type: TransactionType): Flow<List<Transaction>>
     suspend fun insertTransactions(transactions: List<Transaction>)
     suspend fun insertTransaction(transaction: Transaction)
+    suspend fun updateTransaction(transaction: Transaction)
+    suspend fun deleteTransaction(transaction: Transaction)
+    suspend fun getTransactionById(id: Long): Transaction?
 }
