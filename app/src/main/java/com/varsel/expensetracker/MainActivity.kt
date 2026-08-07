5c3b3a2342b46147760c3549009384dbb1b25ff2
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-07 20:45:09 +0530

    Update MainActivity.kt
    fixing nav graph
    [skip ci]

--- app/src/main/java/com/varsel/expensetracker/MainActivity.kt
+++ app/src/main/java/com/varsel/expensetracker/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     // Replace this with your actual root navigation graph or main screen
-                    AppNavGraph()
+                    NavGraph()
                 }
             }
         }
