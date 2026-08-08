70e7d8acb12ca8eefeb348eb7e4bb4fbbc3315c7
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-08 23:48:35 +0530

    Create TransactionInterpreter.kt
    [skip ci]

---
+++ app/src/main/java/com/varsel/expensetracker/parser/TransactionInterpreter.kt
@@ -0,0 +1,14 @@
+package com.varsel.expensetracker.parser
+
+import javax.inject.Inject
+
+class TransactionInterpreter @Inject constructor() {
+
+    fun interpret(
+        block: StatementBlock
+    ): ParsedTransaction {
+
+        TODO("Next step")
+
+    }
+}
