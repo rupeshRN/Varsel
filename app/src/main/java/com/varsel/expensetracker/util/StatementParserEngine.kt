c3fcc388e0fa8d16f4067abe6bbd18681ceb3a9f
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-08 21:42:10 +0530

    Update StatementParserEngine.kt
    [skip ci]

--- app/src/main/java/com/varsel/expensetracker/util/StatementParserEngine.kt
+++ app/src/main/java/com/varsel/expensetracker/util/StatementParserEngine.kt
@@ -2,16 +2,23 @@ package com.varsel.expensetracker.util
 
 import com.varsel.expensetracker.domain.model.Transaction
 import com.varsel.expensetracker.parser.BankDetector
+import com.varsel.expensetracker.parser.TextNormalizer
 import javax.inject.Inject
 
 class StatementParserEngine @Inject constructor(
-    private val bankDetector: BankDetector
+    private val bankDetector: BankDetector,
+    private val textNormalizer: TextNormalizer
 ) {
 
     fun parseStatement(rawText: String): List<Transaction> {
 
-        val parser = bankDetector.detect(rawText)
+        // Normalize extracted PDF text
+        val normalizedText = textNormalizer.normalize(rawText)
 
-        return parser.parse(rawText)
+        // Detect the appropriate parser
+        val parser = bankDetector.detect(normalizedText)
+
+        // Parse transactions
+        return parser.parse(normalizedText)
     }
 }
