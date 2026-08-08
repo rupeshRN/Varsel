d783a67a1ed5999b165f82993d48d90a4885572b
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-08 22:10:20 +0530

    Update StatementParserEngine.kt
    [skip ci]

--- app/src/main/java/com/varsel/expensetracker/util/StatementParserEngine.kt
+++ app/src/main/java/com/varsel/expensetracker/util/StatementParserEngine.kt
@@ -3,32 +3,47 @@ package com.varsel.expensetracker.util
 import com.varsel.expensetracker.domain.model.Transaction
 import com.varsel.expensetracker.parser.BankDetector
 import com.varsel.expensetracker.parser.TextNormalizer
+import com.varsel.expensetracker.parser.TransactionBlockBuilder
 import javax.inject.Inject
 
 class StatementParserEngine @Inject constructor(
     private val bankDetector: BankDetector,
-    private val textNormalizer: TextNormalizer
+    private val textNormalizer: TextNormalizer,
+    private val transactionBlockBuilder: TransactionBlockBuilder
 ) {
 
     fun parseStatement(rawText: String): List<Transaction> {
 
         val normalizedText = textNormalizer.normalize(rawText)
 
+        val blocks = transactionBlockBuilder.build(normalizedText)
+
         throw IllegalArgumentException(
+
             buildString {
 
-                appendLine("========== RAW TEXT ==========")
-                appendLine(rawText)
-
+                appendLine("TOTAL BLOCKS : ${blocks.size}")
                 appendLine()
-                appendLine("========== NORMALIZED ==========")
-                appendLine(normalizedText)
 
-                appendLine()
-                appendLine("========== END ==========")
+                blocks.forEachIndexed { index, block ->
+
+                    appendLine("==============================")
+                    appendLine("BLOCK ${index + 1}")
+                    appendLine("==============================")
+
+                    block.lines.forEach {
+                        appendLine(it)
+                    }
+
+                    appendLine()
+                }
 
             }
+
         )
 
+        // Will be enabled later
+        // val parser = bankDetector.detect(normalizedText)
+        // return parser.parse(normalizedText)
     }
 }
