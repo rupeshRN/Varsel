fd49dca167da086c244048a557f902be5f79b319
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-08 10:06:58 +0530

    Update StatementParserEngineTest.kt
    [skip ci]

--- app/src/test/java/com/varsel/expensetracker/util/StatementParserEngineTest.kt
+++ app/src/test/java/com/varsel/expensetracker/util/StatementParserEngineTest.kt
@@ -6,7 +6,7 @@ import org.junit.Assert.assertFalse
 import org.junit.Test
 
 /**
- * Unit test class for validating bank statement parsing logic with reference numbers.
+ * Unit test class for validating bank statement parsing logic with reference numbers and tabular statements.
  */
 class StatementParserEngineTest {
 
@@ -42,5 +42,29 @@ class StatementParserEngineTest {
         assertEquals(5000.00, secondTransaction.amount, 0.01)
         assertEquals(TransactionType.INCOME, secondTransaction.type)
         assertEquals("UTR: 123456ABC", secondTransaction.referenceNumber?.trim())
+    }
+
+    @Test
+    fun testParseTabularBankStatement() {
+        val tabularStatement = """
+            Value Date Post Date Remitter Branch Description Cheque No DR CR Balance
+            08/05/2023 08/05/2023 ATM SERVICE BRANCH WITHDRAWAL TRANSFER /IMPS COMMISSION CHARGES/312813269279/ TRANSFER TO 88907009396 5.90 14133.70CR
+            08/05/2023 08/05/2023 ATM SERVICE BRANCH WITHDRAWAL TRANSFER UPI/349419035689/NA XXXXX/078905004535@ICIC00007 .ifsc.npciICIC0000789/Sandep Singh TRANSFER TO 97215009396 13000.00 1133.70CR
+        """.trimIndent()
+
+        val parsedTransactions = statementParserEngine.parseStatement(tabularStatement)
+
+        assertFalse("Parsed tabular transactions list should not be empty", parsedTransactions.isEmpty())
+        assertEquals("Expected 2 parsed transactions", 2, parsedTransactions.size)
+
+        val tx1 = parsedTransactions[0]
+        assertEquals(5.90, tx1.amount, 0.01)
+        assertEquals(TransactionType.EXPENSE, tx1.type)
+        assertEquals("312813269279", tx1.referenceNumber)
+
+        val tx2 = parsedTransactions[1]
+        assertEquals(13000.00, tx2.amount, 0.01)
+        assertEquals(TransactionType.EXPENSE, tx2.type)
+        assertEquals("349419035689", tx2.referenceNumber)
     }
 }
