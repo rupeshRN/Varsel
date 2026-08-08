6479ab8fd524c9e9ab492fdbaf921ce9273557f8
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-08 19:39:37 +0530

    Update NavGraph.kt
    [skip ci]

--- app/src/main/java/com/varsel/expensetracker/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/varsel/expensetracker/ui/navigation/NavGraph.kt
@@ -1,6 +1,7 @@
 package com.varsel.expensetracker.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -8,7 +9,6 @@ import com.varsel.expensetracker.ui.category.CategoryScreen
 import com.varsel.expensetracker.ui.dashboard.DashboardScreen
 import com.varsel.expensetracker.ui.import_statement.ImportScreen
 import com.varsel.expensetracker.ui.transaction.TransactionScreen
-import androidx.hilt.navigation.compose.hiltViewModel
 
 sealed class Screen(val route: String) {
     object Dashboard : Screen("dashboard")
@@ -18,39 +18,57 @@ sealed class Screen(val route: String) {
 }
 
 @Composable
-fun NavGraph(navController: NavHostController) {
+fun NavGraph(
+    navController: NavHostController
+) {
+
     NavHost(
         navController = navController,
         startDestination = Screen.Dashboard.route
-    ) 
+    ) {
 
-    composable(Screen.Dashboard.route) {
-    DashboardScreen(
-        viewModel = hiltViewModel(),
-        onNavigateToImport = {
-            navController.navigate(Screen.ImportStatement.route)
-        },
-        onNavigateToAllTransactions = {
-            navController.navigate(Screen.Transactions.route)
-        },
-        onNavigateToCategories = {
-            navController.navigate(Screen.Categories.route)
+        composable(Screen.Dashboard.route) {
+
+            DashboardScreen(
+                viewModel = hiltViewModel(),
+                onNavigateToImport = {
+                    navController.navigate(Screen.ImportStatement.route)
+                },
+                onNavigateToAllTransactions = {
+                    navController.navigate(Screen.Transactions.route)
+                },
+                onNavigateToCategories = {
+                    navController.navigate(Screen.Categories.route)
+                }
+            )
         }
-    )
-    }
+
         composable(Screen.Transactions.route) {
+
             TransactionScreen(
-                onBackClick = { navController.popBackStack() }
+                viewModel = hiltViewModel(),
+                onBackClick = {
+                    navController.popBackStack()
+                }
             )
         }
+
         composable(Screen.Categories.route) {
+
             CategoryScreen(
-                onBackClick = { navController.popBackStack() }
+                viewModel = hiltViewModel(),
+                onBackClick = {
+                    navController.popBackStack()
+                }
             )
         }
+
         composable(Screen.ImportStatement.route) {
+
             ImportScreen(
-                onBackClick = { navController.popBackStack() }
+                onBackClick = {
+                    navController.popBackStack()
+                }
             )
         }
     }
