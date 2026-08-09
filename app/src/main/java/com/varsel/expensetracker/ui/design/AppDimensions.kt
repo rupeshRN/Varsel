6ed0d23092bc9b34bb417f58cae0880219b5c3d2
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-09 19:26:55 +0530

    Create AppDimensions.kt
    new ui [skip ci]

---
+++ app/src/main/java/com/varsel/expensetracker/ui/design/AppDimensions.kt
@@ -0,0 +1,30 @@
+package com.varsel.expensetracker.ui.design
+
+import androidx.compose.ui.unit.dp
+
+object AppDimensions {
+
+    val Space2 = 2.dp
+
+    val Space4 = 4.dp
+
+    val Space8 = 8.dp
+
+    val Space12 = 12.dp
+
+    val Space16 = 16.dp
+
+    val Space20 = 20.dp
+
+    val Space24 = 24.dp
+
+    val Space32 = 32.dp
+
+    val Space40 = 40.dp
+
+    val Space48 = 48.dp
+
+    val CardElevation = 4.dp
+
+    val CardCornerRadius = 16.dp
+}
