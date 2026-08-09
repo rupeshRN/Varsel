40ca2e204f60fe14a5436d91cfe75b553bfe1c33
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-09 22:24:00 +0530

    Create ReportsScreen.kt
    [skip ci]

---
+++ app/src/main/java/com/varsel/expensetracker/ui/reports/ReportsScreen.kt
@@ -0,0 +1,19 @@
+package com.varsel.expensetracker.ui.reports
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ReportsScreen() {
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Text("Reports")
+    }
+}
