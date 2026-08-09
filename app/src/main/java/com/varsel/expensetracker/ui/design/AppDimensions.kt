7210a7fb3776541594673beed732e5d718cb5035
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-09 23:27:00 +0530

    Update AppDimensions.kt
    [skip ci]

--- app/src/main/java/com/varsel/expensetracker/ui/design/AppDimensions.kt
+++ app/src/main/java/com/varsel/expensetracker/ui/design/AppDimensions.kt
@@ -4,37 +4,21 @@ import androidx.compose.ui.unit.dp
 
 object AppDimensions {
 
-    // Screen
+    val ScreenPadding = 20.dp
 
-    val ScreenPadding = 16.dp
+    val ExtraSmallSpacing = 4.dp
 
-    val DialogPadding = 24.dp
+    val SmallSpacing = 8.dp
 
-    val BottomSheetPadding = 20.dp
+    val MediumSpacing = 16.dp
 
-    // Cards
+    val LargeSpacing = 24.dp
 
-    val CardPadding = 16.dp
+    val ExtraLargeSpacing = 32.dp
 
-    val CardSpacing = 12.dp
+    val CardCornerRadius = 20.dp
 
-    val CardCornerRadius = 16.dp
+    val LargeCardCornerRadius = 28.dp
 
-    // Lists
-
-    val ListItemSpacing = 8.dp
-
-    val SectionSpacing = 24.dp
-
-    // Buttons
-
-    val ButtonHeight = 52.dp
-
-    // Icons
-
-    val SmallIcon = 18.dp
-
-    val MediumIcon = 24.dp
-
-    val LargeIcon = 32.dp
+    val CardElevation = 4.dp
 }
