573f038c8762ceb7d201899fac45d0f99e15e38a
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-09 19:33:58 +0530

    Create AppIcons.kt
    [skip ci]

---
+++ app/src/main/java/com/varsel/expensetracker/ui/design/AppIcons.kt
@@ -0,0 +1,39 @@
+package com.varsel.expensetracker.ui.design
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+
+object AppIcons {
+
+    val Dashboard = Icons.Default.Home
+
+    val Transactions = Icons.Default.List
+
+    val Import = Icons.Default.Upload
+
+    val Reports = Icons.Default.BarChart
+
+    val Categories = Icons.Default.Category
+
+    val Settings = Icons.Default.Settings
+
+    val Search = Icons.Default.Search
+
+    val Filter = Icons.Default.FilterList
+
+    val Edit = Icons.Default.Edit
+
+    val Delete = Icons.Default.Delete
+
+    val Add = Icons.Default.Add
+
+    val Income = Icons.Default.ArrowDownward
+
+    val Expense = Icons.Default.ArrowUpward
+
+    val Success = Icons.Default.CheckCircle
+
+    val Warning = Icons.Default.Warning
+
+    val Error = Icons.Default.Error
+}
