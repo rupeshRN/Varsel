42e393fd6eb613ba90e18ec989f92d46646d1ba1
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-09 17:01:14 +0530

    Create ConfidenceReport.kt
    [skip ci]

---
+++ app/src/main/java/com/varsel/expensetracker/parser/ConfidenceReport.kt
@@ -0,0 +1,17 @@
+package com.varsel.expensetracker.parser
+
+data class ConfidenceReport(
+
+    val fields: List<FieldConfidence>
+) {
+
+    val overallScore: Int
+        get() {
+
+            if (fields.isEmpty()) return 0
+
+            return fields.sumOf {
+                it.confidence
+            } / fields.size
+        }
+}
