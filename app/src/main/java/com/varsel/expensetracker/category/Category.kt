6c687c0eb541557986da5b36647fec48aa928e48
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-09 18:43:54 +0530

    Create Category.kt
    [skip ci]

---
+++ app/src/main/java/com/varsel/expensetracker/category/Category.kt
@@ -0,0 +1,34 @@
+package com.varsel.expensetracker.category
+
+object Category {
+
+    const val UNCATEGORIZED = "Uncategorized"
+
+    const val FOOD = "Food"
+
+    const val TRAVEL = "Travel"
+
+    const val SHOPPING = "Shopping"
+
+    const val GROCERIES = "Groceries"
+
+    const val FUEL = "Fuel"
+
+    const val MOBILE = "Mobile"
+
+    const val UTILITIES = "Utilities"
+
+    const val HEALTHCARE = "Healthcare"
+
+    const val ENTERTAINMENT = "Entertainment"
+
+    const val SALARY = "Salary"
+
+    const val TRANSFER = "Transfer"
+
+    const val INVESTMENT = "Investment"
+
+    const val EDUCATION = "Education"
+
+    const val BILLS = "Bills"
+}
