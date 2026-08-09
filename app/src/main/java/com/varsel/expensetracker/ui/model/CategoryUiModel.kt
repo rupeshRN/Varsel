84e891bb3138afba7f06cb0b5aedfe742c43f4c7
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-10 00:51:16 +0530

    Create CategoryUiModel.kt
    [skip ci]

---
+++ app/src/main/java/com/varsel/expensetracker/ui/model/CategoryUiModel.kt
@@ -0,0 +1,14 @@
+package com.varsel.expensetracker.ui.model
+
+import androidx.compose.ui.graphics.Color
+
+data class CategoryUiModel(
+
+    val name: String,
+
+    val emoji: String,
+
+    val backgroundColor: Color,
+
+    val textColor: Color
+)
