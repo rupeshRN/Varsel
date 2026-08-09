a4377a1e8609a9bbc41cbe81cb16a6e88c95d93c
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-09 19:33:30 +0530

    Create ConfidencePalette.kt
    [skip ci]

---
+++ app/src/main/java/com/varsel/expensetracker/ui/design/ConfidencePalette.kt
@@ -0,0 +1,12 @@
+package com.varsel.expensetracker.ui.design
+
+import androidx.compose.ui.graphics.Color
+
+object ConfidencePalette {
+
+    val High = Color(0xFF2E7D32)
+
+    val Medium = Color(0xFFFFA000)
+
+    val Low = Color(0xFFC62828)
+}
