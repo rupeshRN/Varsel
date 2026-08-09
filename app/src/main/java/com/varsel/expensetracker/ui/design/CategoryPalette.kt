087ddc07741b6f088efdd4b631d33f418169f1b5
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-09 19:33:00 +0530

    Create CategoryPalette.kt
    [skip ci]

---
+++ app/src/main/java/com/varsel/expensetracker/ui/design/CategoryPalette.kt
@@ -0,0 +1,26 @@
+package com.varsel.expensetracker.ui.design
+
+import androidx.compose.ui.graphics.Color
+
+object CategoryPalette {
+
+    val Food = Color(0xFFFF9800)
+
+    val Travel = Color(0xFF5E35B1)
+
+    val Shopping = Color(0xFFE91E63)
+
+    val Bills = Color(0xFF6D4C41)
+
+    val Fuel = Color(0xFF00897B)
+
+    val Medical = Color(0xFFD32F2F)
+
+    val Salary = Color(0xFF2E7D32)
+
+    val Investment = Color(0xFF1565C0)
+
+    val Transfer = Color(0xFF7B1FA2)
+
+    val Uncategorized = Color(0xFF757575)
+}
