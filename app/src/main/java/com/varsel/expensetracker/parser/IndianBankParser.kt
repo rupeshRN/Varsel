e0e681051d5c085f5370a33696214036d05c21de
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-10 01:37:09 +0530

    Update IndianBankParser.kt
    [skip ci]

--- app/src/main/java/com/varsel/expensetracker/parser/IndianBankParser.kt
+++ app/src/main/java/com/varsel/expensetracker/parser/IndianBankParser.kt
@@ -16,6 +16,7 @@ class IndianBankParser @Inject constructor(
     private val parserConfidenceEngine: ParserConfidenceEngine,
     private val displayDescriptionBuilder: DisplayDescriptionBuilder,
     private val categoryRuleEngine: CategoryRuleEngine
+    private const val ENABLE_PARSER_DEBUG = false
 ) : StatementParser {
 
     override fun canParse(rawText: String): Boolean {
@@ -114,27 +115,29 @@ class IndianBankParser @Inject constructor(
 // TEMP DEBUG
 //--------------------------------------------------
 
-//throw IllegalArgumentException(
-  //  buildString {
-//
-  //      appendLine("Description : $description")
-    //    appendLine()
-//
-  //      appendLine("Category : ${category.category}")
-    //    appendLine("Category Confidence : ${category.confidence}%")
-      //  appendLine()
-//
-  //      appendLine("Overall Parser Confidence : ${confidence.overallScore}%")
-    //    appendLine()
-//
-  //      confidence.fields.forEach {
-//
-  //          appendLine(
-    //            "${it.field} : ${it.value} (${it.confidence}%)"
-      //      )
-        //}
-    //}
-//)
+if (ENABLE_PARSER_DEBUG)
+            {
+            throw IllegalArgumentException(
+  buildString {
+
+        appendLine("Description : $description")
+        appendLine()
+
+      appendLine("Category : ${category.category}")
+    appendLine("Category Confidence : ${category.confidence}%")
+ appendLine()
+      appendLine("Overall Parser Confidence : ${confidence.overallScore}%")
+    appendLine()
+
+     confidence.fields.forEach {
+
+          appendLine(
+            "${it.field} : ${it.value} (${it.confidence}%)"
+      )
+}
+    }
+)
+            }
 
             //--------------------------------------------------
             // Transaction
