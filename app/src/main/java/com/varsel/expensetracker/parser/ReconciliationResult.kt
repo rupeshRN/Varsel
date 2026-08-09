73683060a1820b7b666b88e476238e4a16ca6ceb
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-09 16:01:29 +0530

    Create ReconciliationResult.kt
    [skip ci]

---
+++ app/src/main/java/com/varsel/expensetracker/parser/ReconciliationResult.kt
@@ -0,0 +1,14 @@
+package com.varsel.expensetracker.parser
+
+data class ReconciliationResult(
+
+    val calculatedCredits: Double,
+
+    val calculatedDebits: Double,
+
+    val creditDifference: Double,
+
+    val debitDifference: Double,
+
+    val isBalanced: Boolean
+)
