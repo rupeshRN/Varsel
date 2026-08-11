227fb72df06cc4fa2dc30cfa74af5d2496754763
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-11 15:58:33 +0530

    Create DeveloperSettings.kt
    [skip ci]

---
+++ app/src/main/java/com/varsel/expensetracker/developer/DeveloperSettings.kt
@@ -0,0 +1,10 @@
+package com.varsel.expensetracker.developer
+
+object DeveloperSettings {
+
+    /**
+     * Turn ON only while developing parser.
+     * Keep FALSE for production.
+     */
+    const val PARSER_DIAGNOSTICS = false
+}
