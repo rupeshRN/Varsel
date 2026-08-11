ecab6a267689b670897180099d496be858c5dfab
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-11 19:17:58 +0530

    Update build.gradle.kts
    [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.datastore.preferences)
 
     // Jetpack Compose (BOM Managed)
     implementation(platform(libs.androidx.compose.bom))
