830ed195e130f79689b6850d2d77a587935978fc
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-14 01:08:37 +0530

    Update BottomActionBar.kt
    [skip ci]

--- app/src/main/java/com/varsel/expensetracker/ui/transaction/components/BottomActionBar.kt
+++ app/src/main/java/com/varsel/expensetracker/ui/transaction/components/BottomActionBar.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun BottomActionBar(
