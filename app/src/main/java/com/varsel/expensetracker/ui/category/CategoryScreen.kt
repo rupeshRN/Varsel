6f0a9668915e4047ef2681906e32e55ba4d52866
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-14 18:18:54 +0530

    Update CategoryScreen.kt
    [skip ci]

--- app/src/main/java/com/varsel/expensetracker/ui/category/CategoryScreen.kt
+++ app/src/main/java/com/varsel/expensetracker/ui/category/CategoryScreen.kt
@@ -119,7 +119,6 @@ fun CategoryScreen(
                     if (newMerchantPattern.isNotBlank() && selectedCategoryForRule.isNotBlank()) {
                         viewModel.addCustomRule(
                             merchantPattern = newMerchantPattern,
-                             displayDescription = merchantPattern
                             categoryName = selectedCategoryForRule
                         )
                         newMerchantPattern = ""
