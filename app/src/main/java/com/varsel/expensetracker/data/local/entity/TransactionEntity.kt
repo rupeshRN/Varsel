556922ebb247a1caaeff99c9b5e9cd06d04ae240
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-15 21:18:39 +0530

    Update TransactionEntity.kt
    [skip ci]

--- app/src/main/java/com/varsel/expensetracker/data/local/entity/TransactionEntity.kt
+++ app/src/main/java/com/varsel/expensetracker/data/local/entity/TransactionEntity.kt
@@ -12,5 +12,6 @@ data class TransactionEntity(
     val description: String,
     val category: String,
     val dateTimestamp: Long,
-    val referenceNumber: String? = null
+    val referenceNumber: String? = null,
+    val transactionFingerprint: String? = null
 )
