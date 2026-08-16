1ba08d5f2dfe29d1f6375cd97ede35291f4931a3
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-16 14:39:22 +0530

    Update AppDatabase.kt
    [skip ci]

--- app/src/main/java/com/varsel/expensetracker/data/local/AppDatabase.kt
+++ app/src/main/java/com/varsel/expensetracker/data/local/AppDatabase.kt
@@ -24,7 +24,7 @@ import javax.inject.Provider
         CustomRuleEntity::class,
         StatementSnapshotEntity::class
     ],
-    version = 4,
+    version = 5,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
@@ -39,26 +39,11 @@ abstract class AppDatabase : RoomDatabase() {
 
     companion object {
 
-val MIGRATION_3_4 = object : Migration(3, 4) {
+val MIGRATION_4_5 = object : Migration(4, 5) {
 
     override fun migrate(
         database: SupportSQLiteDatabase
     ) {
-
-        database.execSQL(
-            """
-            ALTER TABLE transactions
-            ADD COLUMN accountId TEXT
-            """.trimIndent()
-        )
-
-        database.execSQL(
-            """
-            ALTER TABLE transactions
-            ADD COLUMN accountLast4 TEXT
-            """.trimIndent()
-        )
-
         database.execSQL(
             """
             ALTER TABLE statement_snapshots
