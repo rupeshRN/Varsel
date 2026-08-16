d550eae6b975e486a0ccd64e27c60d22b509635e
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-16 14:50:31 +0530

    Update AppDatabase.kt
    [skip ci]

--- app/src/main/java/com/varsel/expensetracker/data/local/AppDatabase.kt
+++ app/src/main/java/com/varsel/expensetracker/data/local/AppDatabase.kt
@@ -39,6 +39,42 @@ abstract class AppDatabase : RoomDatabase() {
 
     companion object {
 
+val MIGRATION_3_4 = object : Migration(3, 4) {
+
+    override fun migrate(
+        database: SupportSQLiteDatabase
+    ) {
+
+        database.execSQL(
+            """
+            ALTER TABLE transactions
+            ADD COLUMN accountId TEXT
+            """.trimIndent()
+        )
+
+        database.execSQL(
+            """
+            ALTER TABLE transactions
+            ADD COLUMN accountLast4 TEXT
+            """.trimIndent()
+        )
+
+        database.execSQL(
+            """
+            ALTER TABLE statement_snapshots
+            ADD COLUMN accountId TEXT
+            """.trimIndent()
+        )
+
+        database.execSQL(
+            """
+            ALTER TABLE statement_snapshots
+            ADD COLUMN accountLast4 TEXT
+            """.trimIndent()
+        )
+    }
+}
+
 val MIGRATION_4_5 = object : Migration(4, 5) {
 
     override fun migrate(
