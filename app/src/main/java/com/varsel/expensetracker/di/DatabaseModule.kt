99a4b8ffb41a1b3c3f3f8d7beacb978440a2f1c9
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-17 18:27:04 +0530

    Update DatabaseModule.kt
    [skip ci]

--- app/src/main/java/com/varsel/expensetracker/di/DatabaseModule.kt
+++ app/src/main/java/com/varsel/expensetracker/di/DatabaseModule.kt
@@ -59,7 +59,8 @@ return Room.databaseBuilder(
     .addMigrations(
         AppDatabase.MIGRATION_3_4,
         AppDatabase.MIGRATION_4_5,
-        AppDatabase.MIGRATION_5_6
+        AppDatabase.MIGRATION_5_6,
+        AppDatabase.MIGRATION_6_7
     )
     .addCallback(
         AppDatabase.SeedCallback(categoryDaoProvider)
