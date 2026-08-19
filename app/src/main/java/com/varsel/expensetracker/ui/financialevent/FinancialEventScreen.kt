60d3fec8f279e246d1e6c0d36867ea55174f27c6
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-19 23:49:59 +0530

    Update FinancialEventScreen.kt
    [skip ci]

--- app/src/main/java/com/varsel/expensetracker/ui/financialevent/FinancialEventScreen.kt
+++ app/src/main/java/com/varsel/expensetracker/ui/financialevent/FinancialEventScreen.kt
@@ -17,9 +17,6 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.ExposedDropdownMenu
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -43,6 +40,9 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 
 @OptIn(ExperimentalMaterial3Api::class)
 private enum class AddTransactionMode {
