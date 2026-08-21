6e0621a68aef74b87d28f2de69ee550065a77a0a
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-21 18:08:51 +0530

    Update build.gradle.kts
    [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.ksp)
+alias(libs.plugins.compose.preview)
 }
 
 android {
@@ -54,6 +55,12 @@ android {
     buildFeatures {
         compose = true
     }
+}
+
+composePreview {
+    variant.set("debug")
+    sdkVersion.set(35)
+    enabled.set(true)
 }
 
 dependencies {
