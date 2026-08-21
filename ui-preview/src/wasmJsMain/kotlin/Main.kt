3579636ff3a30533166cdc0c377c584a146fcec3
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-22 00:03:56 +0530

    Update Main.kt
    [skip ci]

--- ui-preview/src/wasmJsMain/kotlin/Main.kt
+++ ui-preview/src/wasmJsMain/kotlin/Main.kt
@@ -1,8 +1,10 @@
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 
+@OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport(
         viewportContainerId = "ComposeTarget"
