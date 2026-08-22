e74c35d19483d5e2431fca7de73cedb75efabaf4
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-22 22:08:35 +0530

    Update ReportsHeader.kt
    [skip ci]

--- app/src/main/java/com/varsel/expensetracker/ui/reports/components/ReportsHeader.kt
+++ app/src/main/java/com/varsel/expensetracker/ui/reports/components/ReportsHeader.kt
@@ -14,8 +14,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FilterList
-import androidx.compose.material.icons.filled.KeyboardArrowLeft
-import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -111,7 +111,7 @@ fun ReportsHeader(
                     ) {
                         Icon(
                             imageVector =
-                                Icons.Default.KeyboardArrowLeft,
+                                Icons.AutoMirrored.Filled.KeyboardArrowLeft,
                             contentDescription =
                                 "Previous month"
                         )
@@ -132,7 +132,7 @@ fun ReportsHeader(
                     ) {
                         Icon(
                             imageVector =
-                                Icons.Default.KeyboardArrowRight,
+                                Icons.AutoMirrored.Filled.KeyboardArrowRight,
                             contentDescription =
                                 "Next month"
                         )
