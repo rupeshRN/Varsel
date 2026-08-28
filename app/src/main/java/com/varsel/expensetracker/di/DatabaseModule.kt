cd68b76229a58cbb3ae906f5c03f39d6cda31aab
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-29 00:15:05 +0530

    Update DatabaseModule.kt
    [skip ci]

--- app/src/main/java/com/varsel/expensetracker/di/DatabaseModule.kt
+++ app/src/main/java/com/varsel/expensetracker/di/DatabaseModule.kt
@@ -101,7 +101,8 @@ object DatabaseModule {
                 AppDatabase.MIGRATION_6_7,
                 AppDatabase.MIGRATION_7_8,
                 AppDatabase.MIGRATION_8_9,
-                AppDatabase.MIGRATION_9_10
+                AppDatabase.MIGRATION_9_10,
+                AppDatabase.MIGRATION_10_11
             )
             .addCallback(
                 AppDatabase.SeedCallback(
